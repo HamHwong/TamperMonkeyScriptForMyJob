39e7b2a20f851213d663ba3046f169de235426aa
Ham Hwong <ham.huang@elitesource.net>
2018-07-14 15:35:53 +0800

    test tampermonkey update policy

--- plugs.js
+++ plugs.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BMS Vitalize raise ticket plugs
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.11
 // @description  try to take over the world!
 // @author       You
 // @match        *://bmsprod.service-now.com/navpage.do
