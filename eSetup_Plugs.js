5bc10ba08fa47b5473ca3dcb3714a3cbd9aedf4a
HamHuang <HamHwong@gmail.com>
2019-04-10 14:23:56 +0800

    new release

--- eSetup_Plugs.js
+++ eSetup_Plugs.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         eSetup_Plugs
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        http://eprov.bms.com/idm/user/workItemListExt.jsp*
@@ -45,7 +45,7 @@
 
     /**
      * temp
-     * @param {string} url 
+     * @param {string} url
      * @returns {promise}
      */
     function getHtml(url) {
@@ -80,12 +80,12 @@
                     obj[k] = v;
                 }
                 value = obj;
-                console.log(value);
+                //console.log(value);
             }else{
                 key = $(row).find("td:first").text().trim();
                 value = $(row).find("td:nth-child(2)").text().trim();
             }
-            
+
             infoObj[key] = value;
         }
         return infoObj;
@@ -111,7 +111,7 @@
             //到职日期
             if(detailObj.summary["What is the expected start date of the new hire"]){
                 if($("#dzrq").length<=0)
-                $(".tablesortbody").find("tr:first").append(`<th class="tablehdr" nowrap="nowrap" scope="col" id="dzrq">到职日期</th>`)
+                  $(".tablesortbody").find("tr:first").append(`<th class="tablehdr" nowrap="nowrap" scope="col" id="dzrq">到职日期</th>`)
                 var expectedDate = detailObj.summary["What is the expected start date of the new hire"];
                 var datetypeDate = new Date(expectedDate);
                 var nowDate = new Date();
@@ -128,10 +128,45 @@
                 }
                 this.node.append(`<td class='tablebody'></td>`);
             }
-
+            //所选机型
+            if(detailObj.summary["Select the type of equipment needed for your new hire"]){
+                var serial = detailObj.summary["Select the type of equipment needed for your new hire"]
+                  if($("#sxjx").length<=0)
+                  $(".tablesortbody").find("tr:first").append(`<th class="tablehdr" nowrap="nowrap" scope="col" id="sxjx">所选机型</th>`)
+                this.node.append(`<td class='tablebody'>${serial}</td>`);
+            }else{
+                //占位,避免错位
+                if($("#sxjx").length<=0){
+                    $(".tablesortbody").find("tr:first").append(`<th class="tablehdr" nowrap="nowrap" scope="col" id="sxjx">所选机型</th>`);
+                }
+                this.node.append(`<td class='tablebody'></td>`);
+            }
+            //uid
+            var client = detailObj["Client"]||detailObj["客户"];
+            var trimClient = client.replace(/\s+/g,"");
+            var UIDregex = /([(]UID=(.+)[)])/
+            var uid  =UIDregex.exec(trimClient)[2]
+            if($("#uid").length<=0){
+                    $(".tablesortbody").find("tr:first").append(`<th class="tablehdr" nowrap="nowrap" scope="col" id="uid">UID</th>`);
+                this.node.append(`<td class='tablebody' >${uid}</td>`);
+            }else{
+                    this.node.append(`<td class='tablebody' >${uid}</td>`);
+            }
+            //Manager Email
+             var manager = detailObj["Manager"]||detailObj["经理"];
+            var trimManager = manager.replace(/\s+/g,"");
+            var EmailRegex =/\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14}/
+            var email  =EmailRegex.exec(trimManager)[0]
+            if($("#managerEmail").length<=0){
+                    $(".tablesortbody").find("tr:first").append(`<th class="tablehdr" nowrap="nowrap" scope="col" id="managerEmail">经理邮箱</th>`);
+                this.node.append(`<td class='tablebody' >${email}</td>`);
+            }else{
+                    this.node.append(`<td class='tablebody' >${email}</td>`);
+            }
+            //备注
             if(detailObj["Comments"]||detailObj["意见"]){
                 var comments = detailObj["Comments"]||detailObj["意见"];
-                this.node.find("td:first").append(`<div class="comments hidden" style="position:relative;white-space:normal"><textarea style="position:absolute;width:300px;left:0px;top:0px;background-color:#ffffff;">${comments}</span></div>`);
+                this.node.find("td:first").append(`<div class="comments hidden" style="position:relative;white-space:normal"><textarea rows="10" resizeable="false" style="position:absolute;width:300px;left:0px;top:0px;background-color:#ffffff;">${comments}</textarea></div>`);
                 this.node.find("td:first").hover(e=>{
                     this.node.find(".comments").removeClass("hidden").removeClass("shown").addClass("shown");
                 },e=>{
